cafe65f86e25258fdbd53e5abb47a3c9a6f51b99
haojianguo <haojianguo@aibank.com>
2018-11-10 17:52:58 +0800

    init

---
+++ v1/main/block.go
@@ -0,0 +1,30 @@
+package main
+
+import "time"
+
+type Block struct {
+	Version       int64
+	PrevBlockHash []byte
+	MerkelRoot    []byte
+	TimeStamp     int64
+	Bits          int64
+	Nonce         int64
+	Data          []byte
+}
+
+func NewBlock(data string, preBlockHash []byte) *Block {
+	var block Block
+
+	block = Block{
+		Version:       1,
+		PrevBlockHash: preBlockHash,
+		MerkelRoot:    []byte{},
+		TimeStamp:     time.Now().Unix(),
+		Bits:          1,
+		Nonce:         1,
+		Data:          []byte(data),
+	}
+	block.SetHash()
+
+	return &block
+}
---
+++ v1/readme.txt
@@ -0,0 +1,12 @@
+1，定义一个区块的结构Block
+    a，去块头：6个字节
+    b，区块体，字符串表示data
+2，提供一个创建区块的方法
+    NewBlock(参数)
+
+1，定义一个结构体BlockChain
+    Block数组
+2，提供一个创建BlockChain的方法
+    NewBlockChain()
+3，提供一个添加区块的方法
+    AddBlock()
